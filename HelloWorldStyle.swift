73867c14d3922246443ac32cc7fd6505dfd77ee1
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-09-10 15:04:35 +0000

    Fixed Header comment dat

--- HelloWorldStyle.swift
+++ HelloWorldStyle.swift
@@ -2,7 +2,7 @@
 //  HelloWorldStyle.swift
 //
 //  Created by Atri Sarker
-//  Created on 2025-Month-Day
+//  Created on 2025-09-10
 //  Version 1.0
 //  Copyright (c) 2025 Atri Sarker. All rights reserved.
 //
