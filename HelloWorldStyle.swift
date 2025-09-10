2a490e5487ad88b9464f20fc6b9009194fe544e1
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-09-10 14:37:36 +0000

    Added HelloWorldStyle.swift

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,13 @@
+//
+//  HelloWorldStyle.swift
+//
+//  Created by Atri Sarker
+//  Created on 2025-Month-Day
+//  Version 1.0
+//  Copyright (c) 2025 Atri Sarker. All rights reserved.
+//
+//  Program that simply displays Hello World.
+//  File made so that the program is accepted by swiftlint.
+
+// Displays "Hello, World!" but in purple for extra style points.
+print("\033[0;35m Hello, World!")
